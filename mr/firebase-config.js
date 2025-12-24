0f8bac5605007c0cec1c27b9ed85edb227ad4167
ayuh2206-ops <ayuh2206@gmail.com>
2025-12-24 14:51:05 +0530

    Update firebase-config.js

--- mr/firebase-config.js
+++ mr/firebase-config.js
@@ -1,1 +1,21 @@
+// Import Firebase from CDN (No npm install needed)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// Your web app's Firebase configuration
+// REPLACE THESE WITH YOUR REAL KEYS FROM FIREBASE CONSOLE
+const firebaseConfig = {
+  apiKey: "AIzaSyD1hJ4WPm3vTUuNvU4O7MJDfTpbq_SH29U",
+  authDomain: "dnyanjyoti-landing-page.firebaseapp.com",
+  projectId: "dnyanjyoti-landing-page",
+  storageBucket: "dnyanjyoti-landing-page.firebasestorage.app",
+  messagingSenderId: "1084755815698",
+  appId: "1:1084755815698:web:ae3ab3aa88b573fd9ad7cb",
+  measurementId: "G-TVV6GL1DR5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
